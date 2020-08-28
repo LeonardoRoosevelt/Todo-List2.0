864b0ccd09d7bd452e8e7218211ba37b702da7fc
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-08-28 16:34:47 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require("express")
 const app = express()
 const port = 3000
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost/todo-list", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+
+db.on("error", () => {
+  console.log("mongodb error!")
+})
+
+db.once("open", () => {
+  console.log("mongodb success!")
+})
 
 app.get("/", (req, res) => {
   res.send("hello world")
