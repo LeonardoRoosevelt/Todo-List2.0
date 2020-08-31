4fa5518df44fa4bc382645be2a4cfa6f6efc97f0
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-08-31 11:11:50 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const app = express()
 const port = 3000
-const mongoose = require("mongoose")\
+const mongoose = require("mongoose")
 const exphbs = require("express-handlebars")
+// Todo資料
+const Todo = require("./models/todo")
 
 mongoose.connect("mongodb://localhost/todo-list", {
   useNewUrlParser: true,
@@ -19,15 +21,19 @@ db.once("open", () => {
   console.log("mongodb success!")
 })
 
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }))
 
-app.engine('hbs', exphbs({defaultLayout:'main',extname:'hbs'}))
-
-app.set('view engine','hbs')
+app.set("view engine", "hbs")
 
 app.get("/", (req, res) => {
-  res.render("index")
+  //拿到全部的Todo資料
+  Todo.find()
+    .lean()
+    .then(todos => res.render("index", { todos: todos }))
+    .catch(error => console.error(error))
+  // res.render("index")
 })
 
 app.listen(port, () => {
-  console.log("listening on port")
+  console.log(`App is running on http://localhost:${port}`)
 })
