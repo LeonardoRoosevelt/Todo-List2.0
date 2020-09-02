6c26ce53b0e6436cb4b80372cf2791dce74c7ed1
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-09-02 10:06:34 +0800

    feat: add detail page and update todo function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 const mongoose = require("mongoose")
 const exphbs = require("express-handlebars")
+const bodyParser = require("body-parser")
 // Todo資料
 const Todo = require("./models/todo")
 
@@ -24,14 +25,59 @@ db.once("open", () => {
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }))
 
 app.set("view engine", "hbs")
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
-  //拿到全部的Todo資料
+  //拿到全部的Todo資料 => controller  .find, .lean = model  .render = view
   Todo.find()
     .lean()
     .then(todos => res.render("index", { todos: todos }))
     .catch(error => console.error(error))
   // res.render("index")
+})
+
+app.get("/todos/new", (req, res) => {
+  return res.render("new")
+})
+
+app.post("/todos", (req, res) => {
+  const name = req.body.name
+  // const todo = new Todo({ name: name })
+  // return todo
+  //   .save()
+  //   .then(() => res.redirect("/"))
+  //   .catch(error => console.error(error))
+  return Todo.create({ name })
+    .then(() => res.redirect("/"))
+    .catch(error => console.error(error))
+})
+
+app.get("/todos/:id", (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render("detail", { todo }))
+    .catch(error => console.error(error))
+})
+
+app.get("/todos/:id/edit", (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render("edit", { todo }))
+    .catch(error => console.error(error))
+})
+
+app.post("/todos/:id/edit", (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.error(error))
 })
 
 app.listen(port, () => {
