ec1cf55890d182915d587b7dbc68156c61c41f4a
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-09-09 20:49:39 +0800

    refector: modify mongoose config

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
 const express = require("express")
-const app = express()
 const port = 3000
-const mongoose = require("mongoose")
 const exphbs = require("express-handlebars")
 const bodyParser = require("body-parser")
 const methodOverride = require("method-override")
+
 const routes = require("./routes")
+
 // Todo資料
-const Todo = require("./models/todo")
+// const Todo = require("./models/todo")
 
-mongoose.connect("mongodb://localhost/todo-list", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+// const mongoose = require("mongoose")
+// mongoose.connect("mongodb://localhost/todo-list", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
 
-const db = mongoose.connection
+// const db = mongoose.connection
 
-db.on("error", () => {
-  console.log("mongodb error!")
-})
+// db.on("error", () => {
+//   console.log("mongodb error!")
+// })
 
-db.once("open", () => {
-  console.log("mongodb success!")
-})
+// db.once("open", () => {
+//   console.log("mongodb success!")
+// })
+
+require("./config/mongoose")
+
+const app = express()
 
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }))
-
 app.set("view engine", "hbs")
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use(methodOverride("_method"))
-
 app.use(routes)
 
 // app.get("/", (req, res) => {
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,18 +1,19 @@
-const mongoose = require("mongoose")
+// const mongoose = require("mongoose")
 const Todo = require("../todo")
-mongoose.connect("mongodb://localhost/todo-list", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+// mongoose.connect("mongodb://localhost/todo-list", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
 
-const db = mongoose.connection
+const db = require("../../config/mongoose")
 
-db.on("error", () => {
-  console.log("mongodb error!")
-})
+// const db = mongoose.connection
+// db.on("error", () => {
+//   console.log("mongodb error!")
+// })
 
 db.once("open", () => {
-  console.log("mongodb success!")
+  // console.log("mongodb success!")
   for (let i = 0; i < 10; i++) {
     Todo.create({ name: `name-${i}` })
   }
