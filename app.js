d046cacf47c182416862a07b4e24906876f80be2
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-09-09 14:38:27 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose")
 const exphbs = require("express-handlebars")
 const bodyParser = require("body-parser")
 const methodOverride = require("method-override")
+const routes = require("./routes")
 // Todo資料
 const Todo = require("./models/todo")
 
@@ -30,69 +31,71 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(methodOverride("_method"))
 
-app.get("/", (req, res) => {
-  //拿到全部的Todo資料 => controller  .find, .lean = model  .render = view
-  Todo.find()
-    .lean()
-    // asc是順序 desc是倒序
-    .sort({ _id: "asc" })
-    .then(todos => res.render("index", { todos: todos }))
-    .catch(error => console.error(error))
-  // res.render("index")
-})
+app.use(routes)
 
-app.get("/todos/new", (req, res) => {
-  return res.render("new")
-})
+// app.get("/", (req, res) => {
+//   //拿到全部的Todo資料 => controller  .find, .lean = model  .render = view
+//   Todo.find()
+//     .lean()
+//     // asc是順序 desc是倒序
+//     .sort({ _id: "asc" })
+//     .then(todos => res.render("index", { todos: todos }))
+//     .catch(error => console.error(error))
+//   // res.render("index")
+// })
 
-app.post("/todos", (req, res) => {
-  const name = req.body.name
-  // const todo = new Todo({ name: name })
-  // return todo
-  //   .save()
-  //   .then(() => res.redirect("/"))
-  //   .catch(error => console.error(error))
-  return Todo.create({ name })
-    .then(() => res.redirect("/"))
-    .catch(error => console.error(error))
-})
+// app.get("/todos/new", (req, res) => {
+//   return res.render("new")
+// })
 
-app.get("/todos/:id", (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then(todo => res.render("detail", { todo }))
-    .catch(error => console.error(error))
-})
+// app.post("/todos", (req, res) => {
+//   const name = req.body.name
+//   // const todo = new Todo({ name: name })
+//   // return todo
+//   //   .save()
+//   //   .then(() => res.redirect("/"))
+//   //   .catch(error => console.error(error))
+//   return Todo.create({ name })
+//     .then(() => res.redirect("/"))
+//     .catch(error => console.error(error))
+// })
 
-app.get("/todos/:id/edit", (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then(todo => res.render("edit", { todo }))
-    .catch(error => console.error(error))
-})
+// app.get("/todos/:id", (req, res) => {
+//   const id = req.params.id
+//   return Todo.findById(id)
+//     .lean()
+//     .then(todo => res.render("detail", { todo }))
+//     .catch(error => console.error(error))
+// })
 
-app.put("/todos/:id", (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === "on"
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.error(error))
-})
+// app.get("/todos/:id/edit", (req, res) => {
+//   const id = req.params.id
+//   return Todo.findById(id)
+//     .lean()
+//     .then(todo => res.render("edit", { todo }))
+//     .catch(error => console.error(error))
+// })
 
-app.delete("/todos/:id", (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(todo => res.redirect("/"))
-    .catch(error => console.error(error))
-})
+// app.put("/todos/:id", (req, res) => {
+//   const id = req.params.id
+//   const { name, isDone } = req.body
+//   return Todo.findById(id)
+//     .then(todo => {
+//       todo.name = name
+//       todo.isDone = isDone === "on"
+//       return todo.save()
+//     })
+//     .then(() => res.redirect(`/todos/${id}`))
+//     .catch(error => console.error(error))
+// })
+
+// app.delete("/todos/:id", (req, res) => {
+//   const id = req.params.id
+//   return Todo.findById(id)
+//     .then(todo => todo.remove())
+//     .then(todo => res.redirect("/"))
+//     .catch(error => console.error(error))
+// })
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
